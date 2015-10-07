2860fc7087fe638b9319cde02595eb1fc1a4b070
jiangchao0304 <14317048@qq.com>
2015-10-07 23:04:37 +0800

    1

--- bot/src/main/java/com/suzy/bot/App.java
+++ bot/src/main/java/com/suzy/bot/App.java
@@ -1,15 +1,39 @@
 package com.suzy.bot;
 
+import java.io.IOException;
+
+
+
+
+import javax.swing.tree.VariableHeightLayoutCache;
+
+import org.htmlparser.util.ParserException;
+
+import com.suzy.entity.PageResult;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws ParserException, IOException
     {
+    	String url="http://www.apple.com/hk-zh/shop/updateSummary?node=home/shop_iphone/family/iphone6s&step=select&option.dimensionScreensize=4_7inch&option.dimensionColor=silver&option.dimensionCapacity=64gb&option.carrierModel=UNLOCKED%2FWW&carrierPolicyType=UNLOCKED&product=MKQP2ZP%2FA";
+    	
+    	String url2="https://reserve-hk.apple.com/HK/zh_HK/reserve/iPhone?partNumber=MKQJ2ZP%2FA&channel=1&returnURL=http%3A%2F%2Fwww.apple.com%2Fhk-zh%2Fshop%2Fbuy-iphone%2Fiphone6s%2F4.7-%E5%90%8B%E8%9E%A2%E5%B9%95-16gb-%E5%A4%AA%E7%A9%BA%E7%81%B0&sourceID=&iPP=N&appleCare=N&carrier=&store=R485";
     	
     	
-       Reserve.GetStores();
+    	//PageResult result= Reserve.SelectStore(url2);
+       
+    	//if(result.getRedirectUrl()!=null && result.getRedirectUrl().length()>0)
+    	//{
+    	//	PageResult ssPageResult=Reserve.SelectStore(result.getRedirectUrl());
+    	//}
+    	
+    	
+        HttpUtil.sendGet("http://www.tvc-mall.com/User/Login/");
+       HttpUtil.sendPost("http://www.tvc-mall.com/Member/Login", "Email=jiangchao0304%40sina.com&Password=123456&RememberMe=true&RememberMe=false&submit=Login&ReturnUrl=");
+      String ssString= HttpUtil.sendGet("http://www.tvc-mall.com/User/index");
     }
 }
--- bot/src/main/java/com/suzy/bot/Reserve.java
+++ bot/src/main/java/com/suzy/bot/Reserve.java
@@ -1,16 +1,27 @@
 package com.suzy.bot;
 
+import java.io.IOException;
+import java.net.HttpRetryException;
 import java.util.List;
 import java.util.Map;
 
 import com.alibaba.fastjson.JSONObject;
+import com.suzy.entity.PageResult;
 import com.suzy.entity.Stores;
 
+import org.htmlparser.*;
+import org.htmlparser.filters.AndFilter;
+import org.htmlparser.filters.HasAttributeFilter;
+import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+import org.htmlparser.util.ParserUtils;
+import org.htmlparser.visitors.TextExtractingVisitor;
 public class Reserve {
 
-	public static List<Stores> GetStores() {
+	public static List<Stores> GetStores() throws IOException {
 		
-		String jsonString=HttpUtil.sendGet("https://reserve.cdn-apple.com/HK/zh_HK/reserve/iPhone/stores.json", "");
+		String jsonString=HttpUtil.sendGet("https://reserve.cdn-apple.com/HK/zh_HK/reserve/iPhone/stores.json", "").getPageHtml();
 	     
 
 		
@@ -26,9 +37,9 @@ public class Reserve {
 	}
 	
 	
-public static List<Stores> GetAvailability() {
+public static List<Stores> GetAvailability() throws IOException{
 		
-		String jsonString=HttpUtil.sendGet("https://reserve.cdn-apple.com/HK/zh_HK/reserve/iPhone/availability.json", "");
+		String jsonString=HttpUtil.sendGet("https://reserve.cdn-apple.com/HK/zh_HK/reserve/iPhone/availability.json", "").getPageHtml();
 	     
 
 		
@@ -42,4 +53,29 @@ public static List<Stores> GetAvailability() {
 		return listPerson;
 	
 	}
+ 
+
+   public static void Open(String url) throws IOException {
+	
+	   
+	  String firstHtml=  HttpUtil.sendGet(url,"").getPageHtml();
+	  
+	  
+
+	 String productUrl= JSONObject.parseObject(firstHtml).getJSONObject("body").getJSONObject("response").getJSONObject("summarySection").getJSONObject("ireserve")
+	 .getJSONObject("link").getString("url");
+	  
+	 System.out.print(productUrl);
+	   
+   }
+   
+   public static PageResult SelectStore(String url) throws IOException {
+		
+	   
+		  return HttpUtil.sendGet(url,"");
+		  
+		 
+		   
+	   }
+
 }
